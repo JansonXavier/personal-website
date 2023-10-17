575ae2773d5ba4d2503da7ddb7f14e0a8e1a1da9
Janson Xavier <jansonxavier15@gmail.com>
2023-10-17 17:33:44 -0400

    updated dependencies to resolve security alerts. Idk what else tbh

--- utils/data.ts
+++ utils/data.ts
@@ -1,5 +1,3 @@
-import CharacterImg from "../public/Character.png";
-import BoostImg from "../public/Boost.png";
 import { CardType } from "./types";
 
 export const cards: CardType[] = [
@@ -9,7 +7,7 @@ export const cards: CardType[] = [
       "This is the description for Character. I should expect it to be rather long as this is the main thing i'll be showing off",
     technologies: ["React Native", "Firebase"],
     img: {
-      src: CharacterImg,
+      src: "/character.jpg",
       width: 718,
       height: 1446,
     },
@@ -20,10 +18,10 @@ export const cards: CardType[] = [
       "This is the for description Boost, as you can see it does not fit nicely... like at all",
     technologies: ["React", "Excel", "SQL"],
     img: {
-      src: BoostImg,
+      src: "boost.jpg",
       width: 1646,
       height: 1504,
     },
-    url: "http://boost-excel.s3-website-us-east-1.amazonaws.com/",
+    url: "https://boost-splash-page.vercel.app",
   },
 ];
